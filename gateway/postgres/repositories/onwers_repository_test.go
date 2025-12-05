366cc22c0962ca9d547622408d3de925c130e32d
Vitor-Vaz <vitor.vaz.andrade@gmail.com>
2025-12-05 01:03:43 -0300

    chore: add more tests

--- gateway/postgres/repositories/onwers_repository_test.go
+++ gateway/postgres/repositories/onwers_repository_test.go
@@ -3,6 +3,8 @@ package repositories_test
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"mesa-mestre/domain"
 	"mesa-mestre/gateway/postgres/repositories"
 	"testing"
 
@@ -17,22 +19,32 @@ func TestCreateOwner(t *testing.T) {
 		t.Fatalf("failed to connect to database: %v", err)
 	}
 
-	repo := repositories.NewOwnersRepository(db)
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatalf("failed to begin transaction: %v", err)
+	}
+
+	repo := repositories.NewOwnersRepository(tx)
 
 	name := "Michael Scott"
-	email := "michael.scott@dundlermufflinpapers.com"
+	email := "michael.scott@dundlermufflinpapers.com.br"
 
 	t.Run("should create owner sucessfully", func(t *testing.T) {
 		err := repo.CreateOwner(context.Background(), name, email)
 		assert.NoError(t, err)
 
 		var dbName, dbEmail string
-		err = db.QueryRow(`SELECT name, email FROM owners WHERE email = $1`, email).Scan(&dbName, &dbEmail)
-
+		err = tx.QueryRow(`SELECT name, email FROM owners WHERE email = $1`, email).Scan(&dbName, &dbEmail)
 		assert.NoError(t, err)
 
 		assert.Equal(t, name, dbName)
 		assert.Equal(t, email, dbEmail)
+	})
+
+	t.Run("should fail when creating owner with duplicate email", func(t *testing.T) {
+		err := repo.CreateOwner(context.Background(), "Another Name", email)
+		fmt.Println(err)
+		assert.ErrorIs(t, domain.ErrConflict, err)
 	})
 
 }
--- gateway/postgres/repositories/owners_repository.go
+++ gateway/postgres/repositories/owners_repository.go
@@ -2,26 +2,44 @@ package repositories
 
 import (
 	"context"
-	"database/sql"
+	"errors"
+	"mesa-mestre/domain"
 	"mesa-mestre/gateway/postgres/pggen"
+
+	"mesa-mestre/extension/telemetryfs"
+
+	"github.com/lib/pq"
+	"go.uber.org/zap"
 )
 
 type OwnersRepository struct {
-	db *sql.DB
-	q  *pggen.Queries
+	q *pggen.Queries
 }
 
-func NewOwnersRepository(db *sql.DB) *OwnersRepository {
+func NewOwnersRepository(db pggen.DBTX) *OwnersRepository {
 	return &OwnersRepository{
-		db: db,
-		q:  pggen.New(db),
+		q: pggen.New(db),
 	}
 }
 
 // CreateOwner inserts a new owner into the database. name and email are required fields.
 func (r *OwnersRepository) CreateOwner(ctx context.Context, name string, email string) error {
-	return r.q.InsertOwner(ctx, pggen.InsertOwnerParams{
+
+	err := r.q.InsertOwner(ctx, pggen.InsertOwnerParams{
 		Name:  name,
 		Email: email,
 	})
+
+	var pgErr *pq.Error
+
+	if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+		return domain.ErrConflict
+	}
+
+	if err != nil {
+		telemetryfs.Error(ctx, "failed to create owner due to unexpected error", zap.Error(err))
+		return errors.Join(domain.ErrUnexpected, err)
+	}
+
+	return nil
 }

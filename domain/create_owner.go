9b227aa657140482981488c00b252ab0f4207c61
Vitor-Vaz <vitor.vaz.andrade@gmail.com>
2025-12-09 00:55:15 -0300

    adjust domain name

--- domain/create_owner.go
+++ domain/create_owner.go
@@ -6,14 +6,14 @@ type OwnerCreatorRepository interface {
 	CreateOwner(ctx context.Context, name string, email string) error
 }
 
-type OwnerCreator struct {
+type OwnerCreatorUseCase struct {
 	repo OwnerCreatorRepository
 }
 
-func NewOwnerCreator(repo OwnerCreatorRepository) *OwnerCreator {
-	return &OwnerCreator{repo: repo}
+func NewOwnerCreatorUseCase(repo OwnerCreatorRepository) *OwnerCreatorUseCase {
+	return &OwnerCreatorUseCase{repo: repo}
 }
 
-func (oc *OwnerCreator) CreateOwner(ctx context.Context, name string, email string) error {
+func (oc *OwnerCreatorUseCase) CreateOwner(ctx context.Context, name string, email string) error {
 	return oc.repo.CreateOwner(ctx, name, email)
 }

242b37b013a507248505ae0e3e38cba5ef92cca7
Vitor-Vaz <vitor.vaz.andrade@gmail.com>
2026-01-16 01:30:43 -0300

    feat: add some migrations

--- extension/database/priv/migrations/000001_add_dining_tables_table.down.sql
+++ extension/database/priv/migrations/000001_add_dining_tables_table.down.sql
@@ -1,8 +1,1 @@
-CREATE TABLE IF NOT EXISTS dining_tables (
-    id uuid primary key,
-    table_number integer,
-    capacity integer,
-    table_status varchar,
-    created_at timestamp without time zone default now() not null,
-    updated_at timestamp without time zone default now() not null
-);
+DROP TABLE IF EXISTS dining_tables;
